6fc04971592a44ae77b54ec51949240d8c0bf3bc
alfredo.cruz <alfredo.cruz@becasbenitojuarez.gob.mx>
2023-05-15 21:18:47 -0600

    Versión revision 1 cajero

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 
-const btnNumerodeCuenta = document.querySelector("#numeroCuenta");
 const btnIniciarSesion = document.querySelector("#ingresarCuenta");
 const btnCerrarModalSesion = document.querySelector("#cerrarModalSesion");
+const btnDepositar = document.querySelector("#depositar");
+const btnConsultar = document.querySelector("#consultar");
+const btnRetirar = document.querySelector("#retirar");
+const btnTerminar = document.querySelector("#terminar");
+const btnTomarEfectivo = document.querySelector("#tomarEfectivo");
+const btnNumeroCuenta = document.querySelector("#numeroCuenta");
+const btnConfirmaRetiro = document.querySelector("#confirmaRetiro");
+const btnConfirmarDeposito = document.querySelector("#confirmaDeposito");
+
+const alertMensajesEstatus = document.getElementById("mensages");
+const msgBienvenida = document.getElementById("bienvenida");
+const msgSecundario = document.getElementById("mensajeSecundario");
+const msgRetiro = document.getElementById("mensajeRetiro");
+const msgDeposito = document.getElementById("mensajeDeposito");
+
+const MONTO_MINIMO = 10;
+const MONTO_MAXIMO = 990;
+
+const inputRetiroEfectivo = document.getElementById("retiroEfectivo");
+const inputDepositoEfectivo = document.getElementById("depositoEfectivo");
 
 let strNumeroDeCuenta = 0;
+let informacionCuentabiente = {};
 
 let cuentabientes = [
     { cuenta: 3476, nombre: "Mali", saldo: 200, password: 2345, intentosAcceso: 0, bloqueada: false },
@@ -11,28 +31,65 @@ let cuentabientes = [
     { cuenta: 2917, nombre: "Maui", saldo: 67, password: 9628, intentosAcceso: 0, bloqueada: false },
 ];
 
-function retirar(cuenta, cantidad) {
-
+function movimiento(cantidad) {
+    for (let i = 0; i < cuentabientes.length; i++) {
+        if (cuentabientes[i].cuenta === strNumeroDeCuenta) {
+            cuentabientes[i].saldo = cuentabientes[i].saldo + Number(cantidad);
+            informacionCuentabiente = cuentabientes[i];
+            return informacionCuentabiente.saldo;
+        }
+    }
 }
 
-function depositar(cuenta, cantidad) {
+function validarDeposito(cantidad) {
+    mensagesAlerta("¡Deposito de efectivo!", "info");
+    if (cantidad <= 0) {
+        mensagesAlerta("!Solo puede depositar cantidades mayores a CERO!", "warning");
+        inputDepositoEfectivo.value = "";
+        return false;
+    } else if ((informacionCuentabiente.saldo + cantidad) > MONTO_MAXIMO) {
+        mensagesAlerta("!Su saldo final excederia la cantidad maxima permitida, intente con una cantidad menor!", "warning");
+        inputDepositoEfectivo.value = "";
+        return false;
+    } else {
+        return true;
+    }
+}
 
+function validarRetiro(cantidad) {
+    mensagesAlerta("¡Retiro de efectivo!", "info");
+    if (cantidad > informacionCuentabiente.saldo) {
+        mensagesAlerta("!Saldo insuficiete, intente con una cantidad menor!", "warning");
+        inputRetiroEfectivo.value = "";
+        return false;
+    } else if (cantidad <= 0) {
+        mensagesAlerta("!Solo puede retirar cantidades mayores a CERO!", "warning");
+        inputRetiroEfectivo.value = "";
+        return false;
+    } else if ((informacionCuentabiente.saldo - cantidad) <= MONTO_MINIMO) {
+        mensagesAlerta("!Saldo insuficiete, su cuenta no debe tener menos de $" + MONTO_MINIMO + "!", "warning");
+        inputRetiroEfectivo.value = "";
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function validaDatos(numberCenta, nip) {
     let accesoValido = 1; //1. datos validos, 2, cuenta bloqueada, 3. passwor no valido, 4. datos no localizados.
-    for (let detalleCuenta of cuentabientes) {
-        if (detalleCuenta.cuenta === numberCenta && detalleCuenta.intentosAcceso < 3) {
-            if (detalleCuenta.cuenta === numberCenta && detalleCuenta.password === nip) {
+    for (let i = 0; i < cuentabientes.length; i++) {
+        if (cuentabientes[i].cuenta === numberCenta && cuentabientes[i].intentosAcceso < 3) {
+            if (cuentabientes[i].cuenta === numberCenta && cuentabientes[i].password === nip) {
                 accesoValido = 1;
-                strNumeroDeCuenta = detalleCuenta.cuenta;
-            } else if (detalleCuenta.cuenta === numberCenta && detalleCuenta.password != nip) {
-                detalleCuenta.intentosAcceso++;
+                strNumeroDeCuenta = cuentabientes[i].cuenta;
+                informacionCuentabiente = cuentabientes[i];
+            } else if (cuentabientes[i].cuenta === numberCenta && cuentabientes[i].password != nip) {
+                cuentabientes[i].intentosAcceso++;
                 accesoValido = 3;
             }
             break;
-        } else if (detalleCuenta.cuenta === numberCenta && detalleCuenta.intentosAcceso >= 3) {
-            detalleCuenta.bloqueada = true;
+        } else if (cuentabientes[i].cuenta === numberCenta && cuentabientes[i].intentosAcceso >= 3) {
+            cuentabientes[i].bloqueada = true;
             accesoValido = 2;
             break;
         } else {
@@ -43,31 +100,175 @@ function validaDatos(numberCenta, nip) {
     return accesoValido;
 }
 
+function habilitarOperaciones(estatus) {
+    if (estatus) {
+        mensagesAlerta("¡Seleccione la operación que desa realizar en los controles laterales!", "success");
+        msgBienvenida.innerText = "¡Hola " + informacionCuentabiente.nombre + "!";
+        msgSecundario.innerText = "";
+
+        btnDepositar.removeAttribute("hidden");
+        btnConsultar.removeAttribute("hidden");
+        btnRetirar.removeAttribute("hidden");
+        btnTerminar.removeAttribute("hidden");
+        btnTomarEfectivo.setAttribute("hidden", "true");
+        btnNumeroCuenta.removeAttribute("show");
+        btnNumeroCuenta.setAttribute("hidden", "true");
+
+        msgRetiro.removeAttribute("show");
+        msgRetiro.setAttribute("hidden", "true");
+        msgDeposito.removeAttribute("show");
+        msgDeposito.setAttribute("hidden", "true");
+
+        btnTomarEfectivo.removeAttribute("show");
+        btnTomarEfectivo.setAttribute("hidden", "true");
+    } else {
+        informacionCuentabiente = {};
+        mensagesAlerta("¡Sesión cerrada!", "cerrar");
+        msgBienvenida.innerText = "Bienvenido.";
+        msgSecundario.innerText = "Ingrese su número de cuenta.";
+
+        btnDepositar.setAttribute("hidden", "true");
+        btnConsultar.setAttribute("hidden", "true");
+        btnRetirar.setAttribute("hidden", "true");
+        btnTerminar.setAttribute("hidden", "true");
+        btnTomarEfectivo.setAttribute("hidden", "true");
+        btnNumeroCuenta.removeAttribute("hidden");
+        btnNumeroCuenta.setAttribute("show", "true");
+
+        msgRetiro.removeAttribute("show");
+        msgRetiro.setAttribute("hidden", "true");
+        msgDeposito.removeAttribute("show");
+        msgDeposito.setAttribute("hidden", "true");
+
+        btnTomarEfectivo.removeAttribute("show");
+        btnTomarEfectivo.setAttribute("hidden", "true");
+    }
+
+}
+
+function mostrarSaldo() {
+    mensagesAlerta("¡Su saldo disponible es de $" + informacionCuentabiente.saldo + "!", "success");
+    msgBienvenida.innerText = "¡Hola " + informacionCuentabiente.nombre + "!";
+    msgSecundario.innerText = "";
+}
+
 btnIniciarSesion.addEventListener('click', function () {
     let numeroDeCuenta = document.querySelector("#cuentaUsuario");
     let nipUsuario = document.querySelector("#nipUsuario");
     if ((Number(numeroDeCuenta.value) > 0 && Number(numeroDeCuenta.value) != undefined) && (Number(nipUsuario.value) > 0 && Number(nipUsuario.value) != undefined)) {
         switch (validaDatos(Number(numeroDeCuenta.value), Number(nipUsuario.value))) {
             case 1:
-                console.log("Acceso concedido: "+detalleCuenta.nombre);
-                btnCerrarModalSesion.click();
+                habilitarOperaciones(true);
                 break;
             case 2:
-                console.log("Cuenta bloqueada");                
+                mensagesAlerta("¡Su cuenta ha sido bloqueada intente en las proximas 48 horas!", "danger");
+                msgBienvenida.innerText = "";
+                msgSecundario.innerText = "";
                 break;
             case 3:
-                console.log("NIP incorrecto");
+                mensagesAlerta("¡Los datos ingresados no son correctos!", "danger");
+                msgBienvenida.innerText = "";
+                msgSecundario.innerText = "";
                 break;
             case 4:
-                console.log("Ustede no es cuentabiente");                
+                mensagesAlerta("¡Ustede no es cuentabiente!", "danger");
+                msgBienvenida.innerText = "";
+                msgSecundario.innerText = "";
                 break;
             default:
                 console.log("Respuesta desconocida");
-                btnCerrarModalSesion.click();
                 break;
         }
     } else {
-        console.log("Datos de acceso incorrectos...")
+        mensagesAlerta("¡Datos de acceso incorrectos...!", "danger");
+        msgBienvenida.innerText = "";
+        msgSecundario.innerText = "";
+    }
+    btnCerrarModalSesion.click();
+    numeroDeCuenta.value = "";
+    nipUsuario.value = "";
+});
+
+btnTerminar.addEventListener('click', function () {
+    habilitarOperaciones(false);
+});
+
+btnRetirar.addEventListener('click', function () {
+    habilitarOperaciones(true);
+    mensagesAlerta("¡Retiro de efectivo!", "info");
+    msgRetiro.removeAttribute("hidden");
+});
+
+btnDepositar.addEventListener('click', function () {
+    habilitarOperaciones(true);
+    mensagesAlerta("¡Deposito de efectivo!", "info");
+    msgDeposito.removeAttribute("hidden");
+});
+
+btnConfirmaRetiro.addEventListener('click', function () {
+    let retiro = Number(inputRetiroEfectivo.value);
+    if (validarRetiro(retiro)) {
+        btnTomarEfectivo.removeAttribute("hidden");
+        btnTomarEfectivo.setAttribute("show", "true");
     }
 });
 
+btnTomarEfectivo.addEventListener('click', function () {
+    let retiro = inputRetiroEfectivo.value;
+    habilitarOperaciones(true);
+    mensagesAlerta(`¡Su saldo era de $${informacionCuentabiente.saldo}!\nUsted retiro $${retiro}\nSu saldo actual es de $${movimiento((retiro * -1))}`, "warning");
+    inputRetiroEfectivo.value = "";
+});
+
+btnConsultar.addEventListener('click', function () {
+    habilitarOperaciones(true);
+    mostrarSaldo();
+});
+
+btnConfirmarDeposito.addEventListener('click', function () {
+    let deposito = Number(inputDepositoEfectivo.value);
+    if (validarDeposito(deposito)) {
+        habilitarOperaciones(true);
+        mensagesAlerta(`¡Su saldo era de $${informacionCuentabiente.saldo}!\nUsted deposito $${deposito}\nSu saldo actual es de $${movimiento(deposito)}`, "success");
+        inputDepositoEfectivo.value = "";
+    }
+});
+
+btnNumeroCuenta.addEventListener('click', function () {
+    habilitarOperaciones(false);
+});
+
+function mensagesAlerta(texto, tipo) {
+    alertMensajesEstatus.classList = "";
+    alertMensajesEstatus.innerText = texto;
+    alertMensajesEstatus.removeAttribute("hidden");
+    switch (tipo) {
+        case "primary":
+            alertMensajesEstatus.classList = "alert alert-primary position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "secondary":
+            alertMensajesEstatus.classList = "alert alert-secondary position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "success":
+            alertMensajesEstatus.classList = "alert alert-success position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "danger":
+            alertMensajesEstatus.classList = "alert alert-danger position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "warning":
+            alertMensajesEstatus.classList = "alert alert-warning position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "info":
+            alertMensajesEstatus.classList = "alert alert-info position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "light":
+            alertMensajesEstatus.classList = "alert alert-light position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        case "dark":
+            alertMensajesEstatus.classList = "alert alert-dark position-absolute bottom-50  start-50 translate-middle-x";
+            break;
+        default:
+            alertMensajesEstatus.setAttribute("hidden", "true");
+            break;
+    }
+}
